a42498bf602ae6ddf2f854130810410f0bb3e71b
Jena SanCartier and Katy Henning <jena@>
2016-10-11 11:22:47 -0700

    consolidate time and auto update time

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,18 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var datetime = null,
+        date = null;
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('h:mm:ss'));
+};
+
 $(document).ready(function(){
-  $('#time').text(moment());
+    $('#time').text(moment());
+    datetime = $('#time')
+    update();
+    setInterval(update, 1000);
+
 });
 
 },{}]},{},[1]);
--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,3 +1,15 @@
+var datetime = null,
+        date = null;
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('h:mm:ss'));
+};
+
 $(document).ready(function(){
-  $('#time').text(moment());
+    $('#time').text(moment());
+    datetime = $('#time')
+    update();
+    setInterval(update, 1000);
+
 });
