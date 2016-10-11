27152ef517e29d982dbfa8ba0d539035f6846032
Jena SanCartier and Katy Henning <jena@>
2016-10-11 14:30:58 -0700

    add working functions

--- build/js/app.js
+++ build/js/app.js
@@ -2,17 +2,41 @@
 var datetime = null,
         date = null;
 
-var update = function () {
-    date = moment(new Date())
-    datetime.html(date.format('h:mm:ss'));
-};
+// var update = function () {
+//     date = moment(new Date());
+//     datetime.html(date.format('HH:mm'));
+// };
 
 $(document).ready(function(){
-    $('#time').text(moment());
-    datetime = $('#time')
-    update();
-    setInterval(update, 1000);
+var currentTime;
 
+  // update();
+  setInterval(function(){
+    currentTime = new moment().format('hh:mm');
+    $('#time').text(currentTime);
+
+  }, 1000);
+
+  $("#alarmSet").submit(function (event) {
+
+    debugger;
+    var input;
+    var alarmTime;
+    event.preventDefault();
+    input = $('#input').val();
+    alarmTime = new moment(input, "HH:mm").format("HH:mm");
+
+
+    setInterval(function() {
+      var time = new moment().format("HH:mm");
+      console.log("test");
+
+      if (alarmTime === time) {
+        console.log("WAKEUP");
+      }
+    }, 1000);
+
+  });
 });
 
 },{}]},{},[1]);
